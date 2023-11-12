158e605761fce5ac94d49e35ff6527a13a9fa1cc
naieem-bd <naieemur@gmail.com>
2023-11-12 16:33:09 +0600

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import { Button } from '@chakra-ui/react';
+import { Grid, GridItem, Show } from '@chakra-ui/react';
 
 function App() {
-  return <Button colorScheme='blue'>Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}>
+      <GridItem area='nav' bg='pink'>
+        NavBar
+      </GridItem>
+      <Show above='lg'>
+        <GridItem area='aside' bg='gold'>
+          sideBar
+        </GridItem>
+      </Show>
+      <GridItem area='main' bg='coral'>
+        MainContent
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
