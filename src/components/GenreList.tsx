33fa5cc6e094f631ca4b515aeaf1afd884e7b36e
naieem-bd <naieemur@gmail.com>
2023-12-14 18:31:07 +0600

    refactor with react queryu

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -23,7 +23,7 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
 
   return (
     <List>
-      {data.map((genre) => (
+      {data?.results.map((genre) => (
         <ListItem key={genre.id} paddingY='5px'>
           <HStack>
             <Image
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,37 +1,46 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import apiClient from '../services/api-client';
 import { AxiosRequestConfig, CanceledError } from 'axios';
 
-
-interface FetchResponse<T> {
+export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?:any[]) => {
+const useData = <T>(
+  endpoint: string,
+  requestConfig?: AxiosRequestConfig,
+  deps?: any[]
+) => {
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const controller = new AbortController();
+  useEffect(
+    () => {
+      const controller = new AbortController();
 
-    setIsLoading(true);
-    apiClient
-      .get<FetchResponse<T>>(endpoint, { signal: controller.signal, ...requestConfig })
-      .then((res) => {
-        setData(res.data.results);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return
-        setError(err.message);
-        setIsLoading(false);
-      });
-    return () => controller.abort();
-  }, deps ? [...deps] : []);
+      setIsLoading(true);
+      apiClient
+        .get<FetchResponse<T>>(endpoint, {
+          signal: controller.signal,
+          ...requestConfig,
+        })
+        .then((res) => {
+          setData(res.data.results);
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          if (err instanceof CanceledError) return;
+          setError(err.message);
+          setIsLoading(false);
+        });
+      return () => controller.abort();
+    },
+    deps ? [...deps] : []
+  );
 
-  return {isLoading, error, data};
-}
+  return { isLoading, error, data };
+};
 
-export default useData
+export default useData;
