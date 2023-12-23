043c86d6bdf07b346f36a94097eaf025584a4969
Naieemur Rahman <naieemur@gmail.com>
2023-12-23 16:37:50 +0600

    hover effect

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Card,
+  CardBody,
+  HStack,
+  Heading,
+  Image,
+  Text,
+} from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import croppedImgUrl from "../services/img-cropping";
+import { Link } from "react-router-dom";
 
 interface Props {
   game: Game;
@@ -11,13 +20,22 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={5} overflow="hidden">
+    <Card
+      borderRadius={5}
+      overflow="hidden"
+      _hover={{
+        transform: "scale(1.03)",
+        transition: "transform .15s ease-in",
+      }}
+    >
       <div className="cropImg">
         <Image src={croppedImgUrl(game.background_image)} />
         {/* <Image src='https://placehold.co/600x400' /> */}
       </div>
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
+        <Heading fontSize="2xl">
+          <Link to={"/games/" + game.slug}>{game.name}</Link>
+        </Heading>
         <HStack justifyContent="space-between">
           <PlatformIconList
             platforms={game.parent_platforms.map((item) => item.platform)}
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -8,6 +8,7 @@ const apiClient = new APIClient<Game>('/games')
 export interface Game {
   id: number;
   name: string;
+  slug: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
