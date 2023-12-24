7ff59bab98a05cafd0ef9a5699fa85192cae1489
naieem-bd <naieemur@gmail.com>
2023-12-24 14:50:33 +0600

    game info for details page

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,8 +1,11 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import useGame from "../hooks/useGame";
-import { Box, Heading, Spinner, Text } from "@chakra-ui/react";
-import ExpandableText from "../components/ExpandableText";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import useGame from '../hooks/useGame';
+import { Box, Heading, SimpleGrid, Spinner, Text } from '@chakra-ui/react';
+import ExpandableText from '../components/ExpandableText';
+import DefinitionItem from '../components/DefinitionItem';
+import CriticScore from '../components/CriticScore';
+import GameAttributes from '../components/GameAttributes';
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -16,6 +19,7 @@ const GameDetailPage = () => {
     <Box padding={5}>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </Box>
   );
 };
