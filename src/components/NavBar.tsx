083c425da72bfacee2a771c93a0aa24cca46def9
Naieemur Rahman <naieemur@gmail.com>
2024-01-11 07:54:37 +0600

    theme modification

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,15 @@
-import { Box, HStack, Image } from '@chakra-ui/react';
-import logo from '../assets/IGDb_logo.png';
-import ColorModeSwitch from './ColorModeSwitch';
-import SearchInput from './SearchInput';
-import { Link } from 'react-router-dom';
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/IGDb_logo.png";
+import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent='space-between' padding={5}>
-      <Link to='/'>
+    <HStack justifyContent="space-between" padding={5}>
+      <Link to="/">
         <Image src={logo} />
       </Link>
       <SearchInput />
-      <ColorModeSwitch />
     </HStack>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import theme from "./theme";
+import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
@@ -12,8 +11,7 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <ChakraProvider>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
         <ReactQueryDevtools />
