c689d19e59d8dc2fa652880d0f356f5d471add07
Naieemur Rahman <naieemur@gmail.com>
2024-01-13 22:38:40 +0600

    update tsx

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -5,11 +5,11 @@ import {
   MenuButton,
   MenuItem,
   MenuList,
-} from '@chakra-ui/react';
-import { BsChevronBarDown } from 'react-icons/bs';
-import usePlatformName from '../hooks/usePlatformName';
-import usePlatformList from '../hooks/usePlatformList';
-import useGameQueryStore from '../store';
+} from "@chakra-ui/react";
+import { BsChevronBarDown } from "react-icons/bs";
+import usePlatformName from "../hooks/usePlatformName";
+import usePlatformList from "../hooks/usePlatformList";
+import useGameQueryStore from "../store";
 
 const PlatformSelector = () => {
   const { data, error } = usePlatformList();
@@ -20,20 +20,33 @@ const PlatformSelector = () => {
   if (error) return null;
 
   return (
-    <Box mb={2}>
-      <Menu>
-        <MenuButton as={Button} rightIcon={<BsChevronBarDown />}>
-          {selectedPlatform?.name || 'Platforms'}
-        </MenuButton>
-        <MenuList>
-          {data?.results.map((item) => (
-            <MenuItem key={item.id} onClick={() => setPlatformId(item.id)}>
-              {item.name}
-            </MenuItem>
-          ))}
-        </MenuList>
-      </Menu>
-    </Box>
+    <>
+      {/* <Box mb={2}>
+        <Menu>
+          <MenuButton as={Button} rightIcon={<BsChevronBarDown />}>
+            {selectedPlatform?.name || "Platforms"}
+          </MenuButton>
+          <MenuList>
+            {data?.results.map((item) => (
+              <MenuItem key={item.id} onClick={() => setPlatformId(item.id)}>
+                {item.name}
+              </MenuItem>
+            ))}
+          </MenuList>
+        </Menu>
+      </Box> */}
+      <select
+        className="select select-bordered w-full max-w-xs"
+        onChange={(e) => setPlatformId(parseInt(e.target.value))}
+      >
+        <option selected>{selectedPlatform?.name || "Platforms"}</option>
+        {data?.results.map((item) => (
+          <option key={item.id} value={item.id}>
+            {item.name}
+          </option>
+        ))}
+      </select>
+    </>
   );
 };
 
