dc6bceb220fa7259ff420ac199ca38ca67ab56e2
naieem-bd <naieemur@gmail.com>
2024-01-16 18:20:33 +0600

    modify

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,15 @@
-import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/IGDb_logo.png";
-import SearchInput from "./SearchInput";
-import { Link } from "react-router-dom";
+import logo from '../assets/IGDb_logo.png';
+import SearchInput from './SearchInput';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between" padding={5}>
-      <Link to="/">
-        <Image src={logo} />
+    <div className='flex justify-between'>
+      <Link to='/'>
+        <img src={logo} />
       </Link>
       <SearchInput />
-    </HStack>
+    </div>
   );
 };
 
