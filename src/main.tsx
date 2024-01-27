7966f3bd7bcfb2fc9c02ce3756f81b1c83ac23c4
naieem-bd <naieemur@gmail.com>
2024-01-27 11:35:46 +0600

    bug fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { ChakraProvider } from "@chakra-ui/react";
-import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import router from "./routes";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes';
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <ReactQueryDevtools />
-      </QueryClientProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,19 +1,17 @@
-import { Heading, Text } from "@chakra-ui/react";
-import React from "react";
-import { isRouteErrorResponse, useRouteError } from "react-router-dom";
-import NavBar from "../components/NavBar";
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+import NavBar from '../components/NavBar';
 
 const ErrorPage = () => {
   const error = useRouteError();
   return (
     <>
       <NavBar />
-      <Heading>Oops!</Heading>
-      <Text>
+      <h2 className='text-2xl'>Oops!</h2>
+      <p>
         {isRouteErrorResponse(error)
-          ? "This page does not exist."
-          : "An unexpected error occurred."}
-      </Text>
+          ? 'This page does not exist.'
+          : 'An unexpected error occurred.'}
+      </p>
     </>
   );
 };
