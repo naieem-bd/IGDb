66d66dea337666e1bb18872795b5c5ff37257239
naieem-bd <naieemur@gmail.com>
2024-01-27 14:17:33 +0600

    css fix

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,7 +8,7 @@ const NavBar = () => {
       <div className='app-navbar navbar bg-base-200'>
         <div className='flex-1'>
           <Link to='/'>
-            <div className='flex items-center'>
+            <div className='flex items-center app-logo'>
               <img src={logo} className='mr-4' />
               <h1 className='text-3xl font-bold'>Internet Game Database</h1>
             </div>
