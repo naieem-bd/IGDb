78cdeb78b2e03b6d373065a036b49a9ef5b37dde
Naieemur Rahman <naieemur@gmail.com>
2024-01-31 08:37:13 +0600

    file name changed

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
-import Layout from "./pages/Layout";
-import HomePage from "./pages/HomePage";
-import GameDetailPage from "./pages/GameDetailPage";
-import ErrorPage from "./pages/ErrorPage";
+import Layout from "./screens/Layout";
+import HomePage from "./screens/HomePage";
+import GameDetailPage from "./screens/GameDetailPage";
+import ErrorPage from "./screens/ErrorPage";
 
 const router = createBrowserRouter([
   {
