aa6dcb2b2ea5253084d59d208ea29177255153f8
naieem-bd <naieemur@gmail.com>
2024-01-31 16:58:11 +0600

    app name hide in small screen

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,8 +9,10 @@ const NavBar = () => {
         <div className='flex-1'>
           <Link to='/'>
             <div className='flex items-center app-logo'>
-              <img src={logo} className='mr-3' />
-              <h1 className='text-3xl font-bold'>Internet Game Database</h1>
+              <img src={logo} />
+              <h1 className='hidden md:block ml-3 text-3xl font-bold'>
+                Internet Game Database
+              </h1>
             </div>
           </Link>
         </div>
