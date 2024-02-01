0e7a47d25ac50da12025f5370a89394ac93b08a3
Naieemur Rahman <naieemur@gmail.com>
2024-02-01 08:24:14 +0600

    responsive nab

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,15 +6,24 @@ import SideBar from "./SideBar";
 const NavBar = () => {
   return (
     <>
-      <div className="app-navbar navbar bg-base-200 px-3">
-        <div className="drawer block sm:hidden">
+      <div className="app-navbar navbar justify-between bg-base-200 px-3">
+        <div className="drawer inline-block sm:hidden w-auto">
           <input id="my-drawer" type="checkbox" className="drawer-toggle" />
           <div className="drawer-content">
-            <label
-              htmlFor="my-drawer"
-              className="btn btn-primary drawer-button"
-            >
-              Open drawer
+            <label htmlFor="my-drawer" className="btn btn-sm drawer-button">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-5 h-5 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
             </label>
           </div>
           <div className="drawer-side">
@@ -35,11 +44,10 @@ const NavBar = () => {
                 </a>
               </li>
             </ul>
-            <SideBar />
           </div>
         </div>
 
-        <div className="flex-1">
+        <div className="">
           <Link to="/">
             <div className="flex items-center app-logo">
               <img src={logo} />
@@ -49,7 +57,7 @@ const NavBar = () => {
             </div>
           </Link>
         </div>
-        <div className="flex-none gap-2">
+        <div className="">
           <SearchInput />
         </div>
       </div>
