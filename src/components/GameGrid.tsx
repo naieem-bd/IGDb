af6acb039f67e0696fa817f4bae9b0cdb9887e7e
Naieemur Rahman <naieemur@gmail.com>
2024-02-02 08:49:30 +0600

    increase loading skeleton number

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import InfiniteScroll from 'react-infinite-scroll-component';
-import useGameList from '../hooks/useGameList';
-import GameCard from './GameCard';
-import GameCardSkeleton from './GameCardSkeleton';
+import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import useGameList from "../hooks/useGameList";
+import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
   const { isLoading, error, data, fetchNextPage, hasNextPage } = useGameList();
 
-  const skeletons = [1, 2, 3, 4, 5, 6];
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
   const fetchedGamesCount =
     data?.pages.reduce((acc, item) => acc + item.results.length, 0) || 0;
 
@@ -19,8 +19,9 @@ const GameGrid = () => {
         dataLength={fetchedGamesCount}
         hasMore={!!hasNextPage}
         next={() => fetchNextPage()}
-        loader={<span className='loading loading-spinner loading-lg'></span>}>
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6'>
+        loader={<span className="loading loading-spinner loading-lg"></span>}
+      >
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
           {isLoading &&
             skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
           {data?.pages.map((page, index) => (
